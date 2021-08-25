c4103aa65735bc2e571490b40659964f832a5648
Henry <mail@henrygressmann.de>
2021-08-25 11:42:36 +0200

    update eslint config
    Signed-off-by: Henry <mail@henrygressmann.de>

--- pages/_/index.tsx
+++ pages/_/index.tsx
@@ -4,20 +4,19 @@ import Head from 'next/head';
 // NOTE: This is part of an easteregg
 import dynamic from 'next/dynamic';
 const Terminal = dynamic<any>(
-	() => import('../../components/riddle/terminal').then(mod => mod.Terminal),
+	async () =>
+		import('../../components/riddle/terminal').then(mod => mod.Terminal),
 	{ ssr: false },
 );
 
-const Riddle = () => {
-	return (
-		<>
-			<Head>
-				<meta name="robots" content="noindex" />
-			</Head>
-			<Terminal />
-		</>
-	);
-};
+const Riddle = () => (
+	<>
+		<Head>
+			<meta name="robots" content="noindex" />
+		</Head>
+		<Terminal />
+	</>
+);
 
 const Layout: React.FC = ({ children }) => <>{children}</>;
 Riddle.Layout = Layout;
