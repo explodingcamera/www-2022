82a0eff9aa9c98d8ab72fa7578db3c5166517e97
Henry Gressmann <mail@henrygressmann.de>
2022-05-30 18:26:08 +0200

    fix build
    Signed-off-by: Henry Gressmann <mail@henrygressmann.de>

--- next.config.js
+++ next.config.js
@@ -1,18 +1,15 @@
 const withTM = require('next-transpile-modules')(['xterm-for-react']); // Pass the modules you would like to see transpiled
 
+/** @type {import('next').NextConfig} */
 const config = {
-	webpack(config) {
-		// Add Raw-Loader
-		config.module.rules.push({
-			test: /\.css$|\.svg$/,
-			use: 'raw-loader',
-		});
-
-		return config;
-	},
 	poweredByHeader: false,
 	eslint: {
 		ignoreDuringBuilds: true,
+	},
+	reactStrictMode: true,
+	compiler: {
+		styledComponents: true,
+		removeConsole: true,
 	},
 };
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,33 +1,7 @@
 import React from 'react';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
 
 export default class CustomDoc extends Document {
-	static async getInitialProps(ctx) {
-		const sheet = new ServerStyleSheet();
-		const originalRenderPage = ctx.renderPage;
-
-		try {
-			ctx.renderPage = () =>
-				originalRenderPage({
-					enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
-				});
-
-			const initialProps = await Document.getInitialProps(ctx);
-			return {
-				...initialProps,
-				styles: (
-					<>
-						{initialProps.styles}
-						{sheet.getStyleElement()}
-					</>
-				),
-			};
-		} finally {
-			sheet.seal();
-		}
-	}
-
 	render() {
 		return (
 			<Html>
@@ -44,11 +18,25 @@ export default class CustomDoc extends Document {
 					<meta property="og:type" content="website" />
 					<meta property="og:site_name" content="Henry Gressmann" />
 					<meta name="twitter:site" content="@explodingcamera" />
-					<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-					<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-					<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-					<link rel="manifest" href="/site.webmanifest"/>
-					<link rel="icon" type="image/x-icon" href="/favicon.ico"/>
+					<link
+						rel="apple-touch-icon"
+						sizes="180x180"
+						href="/apple-touch-icon.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="32x32"
+						href="/favicon-32x32.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="16x16"
+						href="/favicon-16x16.png"
+					/>
+					<link rel="manifest" href="/site.webmanifest" />
+					<link rel="icon" type="image/x-icon" href="/favicon.ico" />
 				</Head>
 				<body>
 					<Main />
