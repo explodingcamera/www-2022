ca7bda3bdd3cd3145942714d42a45141d08d30cb
explodingcamera <explodingcamera@users.noreply.github.com>
2022-10-25 19:13:17 +0000

    deploy: 468abfc31287eaebc66fc03a4fb188bc604d0487

---
+++ _next/static/chunks/pages/cv-45fad7fe71f61d63.js
@@ -0,0 +1,2 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[920],{7932:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/cv",function(){return t(1338)}])},1338:function(n,u,t){"use strict";t.r(u);var _=t(8650),e=t.n(_),c=t(959);let f=()=>((0,c.useEffect)(()=>{e().push("/")}),null);u.default=f}},function(n){n.O(0,[774,888,179],function(){return n(n.s=7932)}),_N_E=n.O()}]);
+//# sourceMappingURL=cv-45fad7fe71f61d63.js.map
